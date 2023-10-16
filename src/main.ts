8af1163a0d77ef04052994743e138c0ff54f6c98
yosong.heo <ysheo@yeonmu.kr>
2023-10-16 16:03:04 +0900

    jandi header adding

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,11 @@ async function run(): Promise<void> {
     const url: string = core.getInput('url');
     core.info('Sending POST request...');
     const data = JSON.parse(core.getInput('data'));
-    await axios.post(url, data);
+    const header = {
+        "Content-Type": "application/json",
+        "Accept": "application/vnd.tosslab.jandi-v2+json"
+    }
+    await axios.post(url,{headers: header}, data);
   } catch (error) {
     core.setFailed(error.message);
   }
